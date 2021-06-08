8432ef5be7d49e4f573e164e1f8cd0078107605a
Yisel Stephanie <yiselstephanie@gmail.com>
2021-06-08 17:14:13 -0300

    revison tp final

---
+++ index.js
@@ -0,0 +1,17 @@
+const typed = new Typed ('.typed',{
+    strings:[
+        '<i class="entrada">Aventura</i>',
+        '<i class="entrada">Suspenso</i>',
+        '<i class="entrada">Terror</i>',
+        '<i class="entrada">Diversion</i>',
+        '<i class="entrada">Emocion</i>',
+        '<i class="entrada">Romance</i>'
+    ],
+    typeSpeed:35,
+    loop:true,
+    // showCursor:true,
+    cursorChar:'|'
+    // starDelay:200,
+    // backSpeed:80,
+    // smartBackspace:true,
+})
